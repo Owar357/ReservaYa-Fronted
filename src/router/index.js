dcb5759ca0d4a9e37f2c9668209377ce1e0dc950
Marcos <go25001@itcha.edu.sv>
2026-03-13 19:53:46 -0600

    las rutas quitadas

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Login from '@/views/Login.vue'
-import Register from '@/views/Register.vue'
-import AdminEstadisticas from '@/views/AdminEstadisticas.vue'
+'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: Login,
-    },
-    {
-      path: '/AdminEstadisticas',
-      name: 'AdminEstadisticas',
-      component: AdminEstadisticas
-    },
-
-    {
-      path: '/register',
-      name: 'register',
-      component: Register,
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
     {
       path: '/about',
       name: 'about',
